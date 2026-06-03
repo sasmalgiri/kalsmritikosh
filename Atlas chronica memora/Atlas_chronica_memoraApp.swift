fe4a34c3a6cc98f13e7907b9551da1598ca8d487
sasmalgiri <sasmalgiri@gmail.com>
2026-06-04 04:09:49 +0530

    Initial Commit

---
+++ Atlas chronica memora/Atlas_chronica_memoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  Atlas_chronica_memoraApp.swift
+//  Atlas chronica memora
+//
+//  Created by Shirshendu Sasmal on 04/06/26.
+//
+
+import SwiftUI
+
+@main
+struct Atlas_chronica_memoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
